98b642ac747436590e7bc670362a36aee6cd6549
Dmitri Lebedev <ryba4@ryba4.com>
2015-06-01 04:49:02 +0600

    simpler queries + more distances

--- 4.distance-profile.sql
+++ 4.distance-profile.sql
@@ -1,19 +1,18 @@
 ﻿create table if not exists distance_type
 	(name char(20) not null PRIMARY key);
 
-insert into distance_type values ('direct'), ('car'), ('public');
 
 CREATE TABLE if not exists distances
 (
    park integer NOT NULL, 
    house integer NOT NULL, 
-   dtype char(20) not null,
-   distance double precision, 
+   direct_distance double precision, 
+   car_distance double precision,
+   transit_distance double precision,
    population integer,
    PRIMARY KEY (park, house, dtype), 
    FOREIGN KEY (house) REFERENCES houses (id) ON UPDATE NO ACTION ON DELETE cascade, 
-   FOREIGN KEY (park) REFERENCES parks (id) ON UPDATE NO ACTION ON DELETE cascade,
-   FOREIGN KEY (dtype) REFERENCES distance_type (name) ON UPDATE NO ACTION ON DELETE cascade
+   FOREIGN KEY (park) REFERENCES parks (id) ON UPDATE NO ACTION ON DELETE cascade
 );
 delete from distances;
 
@@ -26,58 +25,58 @@ select
 from parks, houses;
 
 
+create type distance_type as enum('direct', 'car', 'transit');
 create table if not exists distance_profile
 (
    park integer NOT NULL, 
    distance double precision not null, 
-   dtype char(20) not null,
+   dtype distance_type,
    people_local integer not null default 0,
    people_total integer not null default 0,
    PRIMARY KEY (park, distance, dtype), 
-   FOREIGN KEY (park) REFERENCES parks (id) ON UPDATE NO ACTION ON DELETE cascade,
-   FOREIGN KEY (dtype) REFERENCES distance_type (name) ON UPDATE NO ACTION ON DELETE cascade
+   FOREIGN KEY (park) REFERENCES parks (id) ON UPDATE NO ACTION ON DELETE cascade
 );
 delete from distance_profile;
 
 insert into distance_profile
 	(park, distance, dtype)
 with
-	x as (select park, max(distance::int) md, dtype from distances group by park, dtype)
+	x as (select park, max(distance::int) md, 'direct' from distances group by park, dtype)
 select park, generate_series(250, md, 250) dst, dtype from x;
 
 with
 	local_data as (
 		select
-			a.park, a.distance, a.dtype, sum(b.population) pop
+			a.park, a.distance, sum(b.population) pop
 		from
 			distance_profile a, distances b
 		where
 			a.park = b.park
 			and b.distance <= a.distance
 			and b.distance >= a.distance - 250
-		group by a.park, a.distance, a.dtype
+		group by a.park, a.distance
 	)
 update distance_profile dp
 set people_local = local_data.pop
 from local_data
 where dp.park = local_data.park
-	and dp.dtype = local_data.dtype
+	and dp.dtype = 'direct'
 	and dp.distance = local_data.distance;
 
 with
 	total_data as (
 		select
-			a.park, a.distance, a.dtype, sum(b.population) pop
+			a.park, a.distance, sum(b.population) pop
 		from
 			distance_profile a, distances b
 		where
 			a.park = b.park
 			and b.distance <= a.distance
-		group by a.park, a.distance, a.dtype
+		group by a.park, a.distance
 	)
 update distance_profile dp
 set people_total = td.pop
 from total_data td
 where dp.park = td.park
 	and dp.distance = td.distance
-	and dp.dtype = td.dtype;
+	and dp.dtype = 'direct';
