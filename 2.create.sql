4ecc917f755041f44837714649739b1cbf2388a7
Dmitri Lebedev <ryba4@ryba4.com>
2015-05-31 17:47:43 +0600

    added more parks

--- 2.create.sql
+++ 2.create.sql
@@ -1,5 +1,4 @@
-DROP TABLE IF EXISTS parks;
-CREATE TABLE parks (
+CREATE TABLE if not exists parks (
     id serial primary key,
     osm_id integer unique,
     name character varying(255),
@@ -7,8 +6,16 @@ CREATE TABLE parks (
     contour_913 geometry
 );
 
+delete from parks;
+
 insert into parks ( osm_id, name, contour, contour_913 )
 	select osm_id, name, way, st_transform(way, 900913)
 		from osm_polygon
-		where osm_id in (25642909, 25673117, 25673144);
+		where osm_id in (25642909,
+			25673117,
+			25673144,
+			36774872,
+			25768832,
+			25657738,
+			290224818);
 
