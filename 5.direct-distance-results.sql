d31029c2537c86a2082837be23fc39fcbf3c98c8
Dmitri Lebedev <ryba4@ryba4.com>
2015-06-01 01:22:28 +0600

    many changes

--- 5.direct-distance-results.sql
+++ 5.direct-distance-results.sql
@@ -3,3 +3,12 @@ create view distance_profile_results as
 	select p.id, p.osm_id, p.name, dp.dtype, dp.distance, dp.people_local, dp.people_total
 	from distance_profile dp, parks p
 	where dp.park = p.id;
+
+drop view if exists average_distance;
+create view average_distance as
+	with
+		totals as (select sum(population) totalpop from houses),
+
+	select park, sum(distance * people) / totalpop
+	from distances d, totals t
+	group by park;
