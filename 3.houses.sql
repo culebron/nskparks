7883c7deea96b569d5b1d87098db33d23b8791d8
Dmitri Lebedev <ryba4@ryba4.com>
2015-05-31 17:48:05 +0600

    added distance type dimension

--- 3.houses.sql
+++ 3.houses.sql
@@ -1,8 +1,9 @@
-DROP TABLE IF EXISTS houses;
-create table houses (
+create table if not exists houses (
 	id serial primary key,
 	query char(100),
 	centroid geometry,
 	centroid_913 geometry,
 	population integer
 );
+
+delete from houses;
--- 4.distance-profile.sql
+++ 4.distance-profile.sql
@@ -1,18 +1,25 @@
-﻿CREATE TABLE if not exists distances
+﻿create table if not exists distance_type
+	(name char(20) not null PRIMARY key);
+
+insert into distance_type values ('direct'), ('car'), ('public');
+
+CREATE TABLE if not exists distances
 (
    park integer NOT NULL, 
    house integer NOT NULL, 
+   dtype char(20) not null,
    distance double precision, 
    population integer,
-   PRIMARY KEY (park, house), 
+   PRIMARY KEY (park, house, dtype), 
    FOREIGN KEY (house) REFERENCES houses (id) ON UPDATE NO ACTION ON DELETE cascade, 
-   FOREIGN KEY (park) REFERENCES parks (id) ON UPDATE NO ACTION ON DELETE cascade
+   FOREIGN KEY (park) REFERENCES parks (id) ON UPDATE NO ACTION ON DELETE cascade,
+   FOREIGN KEY (dtype) REFERENCES distance_type (name) ON UPDATE NO ACTION ON DELETE cascade
 );
 delete from distances;
-	
+
 insert into distances
 select
-	parks.id, houses.id, 
+	parks.id, houses.id, 'direct',
 	(parks.contour_913 <-> houses.centroid_913)
 	* cos(radians(st_y(st_setsrid(houses.centroid, 4326)))),
 	population
@@ -23,50 +30,54 @@ create table if not exists distance_profile
 (
    park integer NOT NULL, 
    distance double precision not null, 
+   dtype char(20) not null,
    people_local integer not null default 0,
    people_total integer not null default 0,
-   PRIMARY KEY (park, distance), 
-   FOREIGN KEY (park) REFERENCES parks (id) ON UPDATE NO ACTION ON DELETE cascade
+   PRIMARY KEY (park, distance, dtype), 
+   FOREIGN KEY (park) REFERENCES parks (id) ON UPDATE NO ACTION ON DELETE cascade,
+   FOREIGN KEY (dtype) REFERENCES distance_type (name) ON UPDATE NO ACTION ON DELETE cascade
 );
 delete from distance_profile;
 
 insert into distance_profile
-	(park, distance)
+	(park, distance, dtype)
 with
-	x as (select park, max(distance::int) md from distances group by park)
-select x.park, generate_series(250, x.md, 250) dst from x;
+	x as (select park, max(distance::int) md, dtype from distances group by park, dtype)
+select park, generate_series(250, md, 250) dst, dtype from x;
 
 with
 	local_data as (
 		select
-			a.park, a.distance, sum(b.population) pop
+			a.park, a.distance, a.dtype, sum(b.population) pop
 		from
 			distance_profile a, distances b
 		where
 			a.park = b.park
 			and b.distance <= a.distance
 			and b.distance >= a.distance - 250
-		group by a.park, a.distance
+		group by a.park, a.distance, a.dtype
 	)
 update distance_profile dp
 set people_local = local_data.pop
 from local_data
 where dp.park = local_data.park
- and dp.distance = local_data.distance;
+	and dp.dtype = local_data.dtype
+	and dp.distance = local_data.distance;
 
 with
 	total_data as (
 		select
-			a.park, a.distance, sum(b.population) pop
+			a.park, a.distance, a.dtype, sum(b.population) pop
 		from
 			distance_profile a, distances b
 		where
 			a.park = b.park
 			and b.distance <= a.distance
-		group by a.park, a.distance
+		group by a.park, a.distance, a.dtype
 	)
 update distance_profile dp
 set people_total = td.pop
 from total_data td
-where dp.park = td.park and dp.distance = td.distance;
-
+where dp.park = td.park
+	and dp.distance = td.distance
+	and dp.dtype = td.dtype;
